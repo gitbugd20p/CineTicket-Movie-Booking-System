98a72a9bd0a89e98c72b59c27a26e6b92f322d2c
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-01-29 22:15:15 +0600

    feat: implement navbar with Clerk authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "./../assets/assets";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
 import { useState } from "react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
+  const navigate = useNavigate();
 
   return (
     <div className="fixed top-0 left-0 z-50 flex w-full items-center justify-between px-6 py-5 md:px-16 lg:px-36">
@@ -37,9 +43,24 @@ const Navbar = () => {
 
       <div className="flex items-center gap-8">
         <SearchIcon className="h-8 w-8 cursor-pointer max-md:hidden" />
-        <button className="bg-primary hover:bg-primary-dull cursor-pointer rounded-full px-4 py-1 font-medium transition sm:px-7 sm:py-2">
-          Login
-        </button>
+        {!user ? (
+          <button
+            className="bg-primary hover:bg-primary-dull cursor-pointer rounded-full px-4 py-1 font-medium transition sm:px-7 sm:py-2"
+            onClick={openSignIn}
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
 
       <MenuIcon
